9966c39f353b4b48d5fb44e0d06fa4e2b760fd65
Oleksii Zuiev <oleksii.zuiev@youscan.io>
2020-07-10 16:23:10 +0300

    Solve excercises 1.2-2 & 1.2-3

---
+++ Chapter1/Chapter1Excercises/Chapter1Excercises/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Chapter1Excercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.2-2
+            var n = ConvergesAt(2, x => 64 * x * Math.Log(x), x => 8 * x * x);
+            Console.WriteLine($"Coverges at {n}");
+            
+            //1.2-3
+            n = ConvergesAt(1, x => 100 * x * x, x => Math.Pow(2, x));
+            Console.WriteLine($"Coverges at {n}");
+        }
+
+        private static int ConvergesAt(int startN, Func<int, double> f1, Func<int, double> f2)
+        {
+            var n = startN;
+            var initiallyF1Greater = f1(n) > f2(n);
+
+            bool f1Greater;
+            do
+            {
+                n++;
+                f1Greater = f1(n) > f2(n);
+            } while (f1Greater == initiallyF1Greater);
+
+            return n;
+        }
+    }
+}
