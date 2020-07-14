d13c5a6b82ea554cdd2fa9d59ed2c4d438c60467
Oleksii Zuiev <oleksii.zuiev@youscan.io>
2020-07-14 10:29:45 +0300

    Problem 1-1

--- Chapter1/Chapter1Excercises/Chapter1Excercises/Program.cs
+++ Chapter1/Chapter1Excercises/Chapter1Excercises/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Chapter1Excercises
 {
@@ -14,6 +15,9 @@ namespace Chapter1Excercises
             //1.2-3
             n = ConvergesAt(1, x => 100 * x * x, x => Math.Pow(2, x));
             Console.WriteLine($"Coverges at {n}");
+            
+            //1-1
+            RunningTimeComparison();
         }
 
         private static int ConvergesAt(int startN, Func<int, double> f1, Func<int, double> f2)
@@ -29,6 +33,61 @@ namespace Chapter1Excercises
             } while (f1Greater == initiallyF1Greater);
 
             return n;
+        }
+
+        private static void RunningTimeComparison()
+        {
+            (double microseconds, string name)[] intervals = new (double microseconds, string name)[]
+            {
+                (microseconds: 1_000_000 * 1, name: "1 second"), 
+                (microseconds: 1_000_000 * 60, name: "1 minute"), 
+                (microseconds: 1_000_000.0 * 60 * 60, "1 hour"), 
+                (microseconds: 1_000_000.0 * 60 * 60 * 24, "1 day"),
+                (microseconds: 1_000_000.0 * 60 * 60 * 24 * 30.438, "1 month"),
+                (microseconds: 1_000_000.0 * 60 * 60 * 24 * 365.25, "1 year"),
+                (microseconds: 1_000_000.0 * 60 * 60 * 24 * 365.25 * 100, "1 century")
+            };
+
+            (Func<double, double> func, string name)[] functions = new(Func<double, double> func, string name)[]
+            {
+                (func: x => x * Math.Log(x), "n * ln(n)"),
+                (func: x => x * x, "n ^ 2"),
+                (func: x => x * x * x, "n ^ 3"),
+                (func: x => Math.Pow(2, x), "2 ^ n"),
+                (func: x => Factorial((int)x), "n!")
+            };
+            
+            //for lg n - e ^ microseconds
+            //for sqrt(n) - n ^ 2
+
+
+            foreach (var function in functions)
+            {
+                double n = 1;
+                int currentInterval = 0;
+                while (currentInterval < intervals.Length)
+                {
+                    var nMicroseconds = function.func(n);
+                    if (nMicroseconds > intervals[currentInterval].microseconds)
+                    {
+                        Console.WriteLine($"Function {function.name} can perform {n-1} operations within {intervals[currentInterval].name}");
+                        currentInterval++;
+                    }
+
+                    n += 1000;
+                }
+            }
+        }
+
+        static double Factorial(int number)
+        {
+            double result = 1;
+            while (number != 1)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+            return result;
         }
     }
 }
