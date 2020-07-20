d2b60ed4e5869ef43968a2b34ea01ae71bcec2c1
Oleksii Zuiev <oleksii.zuiev@youscan.io>
2020-07-20 10:14:34 +0300

    Add solution for excercise 2.1-1

---
+++ Chapter2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Chapter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new [] { 31, 41, 59, 26, 41, 58};
+            InsertionSortDesc(array);
+            Console.WriteLine(string.Join(",", array));
+        }
+
+        static void InsertionSortDesc(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                var key = array[i];
+                var j = i - 1;
+                while (j >= 0 && array[j] < key)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+
+                array[j + 1] = key;
+            }
+        }
+    }
+}
